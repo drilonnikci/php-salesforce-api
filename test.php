16787c6bf8897b36cf790047e525955c3c254e07
drilonnikci <drilonnikci@gmail.com>
2023-09-28 15:47:58 +0200

    removed keys

--- test.php
+++ test.php
@@ -8,10 +8,10 @@ use drilonnikci\PhpSalesforceApi\Authentication\SalesforceAuthRequest;
 
 $credentials = [
     'grant_type' => 'password',
-    'client_id' => '3MVG9ux34Ig8G5eoM6hLdtcIAg2WyWEmlMao7B9h8iZqTnAwfPhc5bWkKtaT9T6qYXl3LJ3AuTE0Dk0qbFqvf',
-    'client_secret' => 'B017B6D35DF92B37E29A4DA96D3BC460EAF3A745399EBD37CA9E2CF1116B854C',
-    'username' => 'drilon@cydeo.com',
-    'password' => '@SF.cydeo2023@new',
+    'client_id' => 'YOUR_CONSUMER_KEY',
+    'client_secret' => 'YOUR_CONSUMER_SECRET',
+    'username' => 'YOUR_USERNAME',
+    'password' => 'YOUR_PASSWORD',
 ];
 
 $authentication = new SalesforceAuthRequest($credentials, false); // set true to use sandbox false for production
