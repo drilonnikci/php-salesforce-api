a695a59c75b251895229804dced63197b27b6497
drilonnikci <drilonnikci@gmail.com>
2023-10-04 16:08:42 +0200

    Added update method

--- src/SObject.php
+++ src/SObject.php
@@ -12,6 +12,7 @@ class SObject
     private string $sobjectUrl;
     private string $dataUrl;
     private array $headers;
+    private Client $httpClient;
 
     use LoggerTrait;
 
@@ -21,14 +22,21 @@ class SObject
         $this->dataUrl = "{$instanceUrl}/services/data/v58.0";
         $this->sobjectUrl = $this->dataUrl . '/sobjects';
         $this->headers = $this->salesforce->getHeaders();
+        $this->httpClient = new Client(['headers' => $this->headers]);
     }
 
-    public function create($object, $data): false|string
+
+    /**
+     * @param string $object
+     * @param array $data
+     *  Create a record on Salesforce e.g. Lead, Opportunity
+     * @return false|string
+     */
+    public function create(string $object, array $data): false|string
     {
         try {
-            $client = new Client();
             $uri = $this->sobjectUrl . "/{$object}";
-            $response = $client->post($uri, ['headers' => $this->headers, 'json' => $data]);
+            $response = $this->httpClient->post($uri, ['headers' => $this->headers, 'json' => $data]);
             $message = [
                 'status' => $response->getReasonPhrase(),
                 'code' => $response->getStatusCode(),
@@ -43,6 +51,37 @@ class SObject
                 'message' => $exception->getMessage(),
             ];
             $this->getLogger()->error("{$object} wasn't created. ",  $message);
+        }
+
+        return json_encode($message);
+    }
+
+    /**
+     * @param string $object
+     * @param string $recordId
+     * @param array $data
+     * Update record on salesforce
+     * @return false|string
+     */
+    public function update(string $object, string $recordId, array $data): false|string
+    {
+        try {
+            $uri = $this->sobjectUrl . "/{$object}/{$recordId}";
+            $response = $this->httpClient->patch($uri, ['json' => $data]);
+            $message = [
+                'status' => $response->getReasonPhrase(),
+                'code' => $response->getStatusCode(),
+                'body' => json_decode($response->getBody()),
+            ];
+
+            $this->getLogger()->notice("{$object} with id {$recordId} updated", $message);
+        } catch (GuzzleException $exception) {
+            $message = [
+                'status' => 'Failed',
+                'code' => $exception->getCode(),
+                'message' => $exception->getMessage(),
+            ];
+            $this->getLogger()->error("{$object} with id {$recordId} wasn't updated. ",  $message);
         }
 
         return json_encode($message);
